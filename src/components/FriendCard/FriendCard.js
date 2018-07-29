0cc8ce209a58703b7ef683e8760cd9900b5cb5fa
Pamela Yang <pamelapyang@hotmail.com>
2018-07-29 00:10:13 -0500

    fixed images

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,13 +1,14 @@
 import React from "react";
+// import { Container, Row, Col } from 'reactstrap';
 import "./FriendCard.css";
 
 const FriendCard = (props) => (
-  <div className="card">
-    <div className="img-container">
+<div className="col-xs-6 col-sm-6 col-md-4 container">
+    <div className="img-box">
       <img
         src={ props.image }
       />
-    </div>
+   </div>
   </div>
 );
 
